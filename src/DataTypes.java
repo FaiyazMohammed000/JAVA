de5ae60f50929c5b6c5a3e68a45e0b9718b7d054
Mohammed Faiyaz <mohdfaiyaz2000@gmail.com>
2023-02-06 19:00:21 +0530

    data tpes added

---
+++ src/DataTypes.java
@@ -0,0 +1,29 @@
+public class DataTypes {
+    public static void main(String[]args)
+    {
+        int a=789;
+        char b='G';
+        float c=0.2F;
+        double d=7.86489546598;
+        String s="hash d h fhuf dfguf";
+        boolean e=true;
+        short f=3;
+        long l=634285;
+        byte j=127;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(s);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(l);
+        System.out.println(j);
+
+
+
+
+
+
+    }
+}
